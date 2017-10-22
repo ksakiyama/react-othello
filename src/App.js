3116892b2018e8e8a05e27c50a0de50e539f250c
ksakiyama <k.sakiyama1@gmail.com>
2017-10-22 20:11:09 +0900

    added searchReversable

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ class App extends Component {
       turn: 0,
       user: B // first player
     };
+
+    this.searchReversable(this.state.user);
   }
 
   getCurrentBoardArray(y, x) {
@@ -41,12 +43,67 @@ class App extends Component {
   }
 
   clickHandler(e, key) {
+    console.log("clicked");
     const y = Number(key.charAt(0));
     const x = Number(key.charAt(1));
     const board = this.state.board;
-    if (board[y][x] === E) {
-      this.reverse(y, x)      
+
+    // if (board[y][x] === P) {
+    if (this.reverse(y, x)) {
+      console.log("debug");
+      this.searchReversable(); // なぜかreverseで更新されたuserが変更されていない
     }
+    // }
+
+    console.log("clicked end");
+  }
+
+  searchReversable(user) {
+    var board = this.state.board;
+    for (var y = 0; y < 8; y++) {
+      for (var x = 0; x < 8; x++) {
+        if (board[y][x] === B || board[y][x] === W) {
+          continue;
+        }
+
+        board[y][x] = E;
+
+        for (var dy = -1; dy <= 1; dy++) {
+          for (var dx = -1; dx <= 1; dx++) {
+            if (dx === 0 && dy === 0) {
+              continue;
+            }
+
+            var sx = x + dx;
+            var sy = y + dy;
+            if (!this.onBoard(sy, sx) || !(board[sy][sx] === 1 - user)) {
+              continue;
+            }
+
+            while (true) {
+              sx = sx + dx;
+              sy = sy + dy;
+              if (
+                !this.onBoard(sy, sx) ||
+                board[sy][sx] === E ||
+                board[sy][sx] === P
+              ) {
+                break;
+              } else if (board[sy][sx] === 1 - user) {
+                continue;
+              } else {
+                board[y][x] = P;
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    this.setState({
+      board: board
+    });
   }
 
   reverse(y, x) {
@@ -67,10 +124,14 @@ class App extends Component {
           continue;
         }
 
-        while(true) {
+        while (true) {
           sx = sx + dx;
           sy = sy + dy;
-          if (!this.onBoard(sy, sx) || board[sy][sx] === E) {
+          if (
+            !this.onBoard(sy, sx) ||
+            board[sy][sx] === P ||
+            board[sy][sx] === E
+          ) {
             break;
           } else if (board[sy][sx] === this.getNextUser()) {
             continue;
@@ -91,10 +152,18 @@ class App extends Component {
     }
 
     if (flag) {
+      // refresh
+      // for (var iy = 0; iy < 8; iy++) {
+      //   for (var ix = 0; ix < 8; ix++) {
+      //     if (board[iy][ix] === P) {
+      //       board[iy][ix] = E;
+      //     }
+      //   }
+      // }
       this.setState({
         board: board,
         turn: this.state.turn++,
-        user: this.getNextUser()
+        user: 1 - this.state.user
       });
     }
 
@@ -124,6 +193,16 @@ class App extends Component {
   }
 
   render() {
+    // debug
+    const user = this.state.user;
+    const turn = this.state.turn;
+    if (user === B) {
+      console.log("user:black");
+    } else {
+      console.log("user:white");
+    }
+    console.log("turn:" + turn);
+
     let list = [];
     for (var y = 0; y < 8; y++) {
       for (var x = 0; x < 8; x++) {
@@ -132,9 +211,6 @@ class App extends Component {
       const key = "br" + y;
       list.push(<br key={key} />);
     }
-
-    // debug
-    console.log("called render()");
 
     return <div className="App">{list}</div>;
   }
