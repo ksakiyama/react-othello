87ea7f3047302cd38ad03b3b6ace19f51a2b5c04
ksakiyama <k.sakiyama1@gmail.com>
2018-09-13 05:40:16 +0900

    use Stateless functional component

--- src/BoardLine.js
+++ src/BoardLine.js
@@ -1,24 +1,24 @@
 import React from "react";
 import Cell from "./Cell.js";
 
-export default class BoardLine extends React.Component {
-  render() {
-    const y = this.props.y;
-    return (
-      <tr>
-        <th>{y + 1}</th>
-        {this.props.line.map((cellType, x) => {
-          return (
-            <Cell
-              key={"" + y + x}
-              x={x}
-              y={y}
-              clickHandler={this.props.clickHandler}
-              type={cellType}
-            />
-          );
-        })}
-      </tr>
-    );
-  }
+const BoardLine = (props) => {
+  const y = props.y;
+  return (
+    <tr>
+      <th>{y + 1}</th>
+      {props.line.map((cellType, x) => {
+        return (
+          <Cell
+            key={"" + y + x}
+            x={x}
+            y={y}
+            clickHandler={props.clickHandler}
+            type={cellType}
+          />
+        );
+      })}
+    </tr>
+  )
 }
+
+export default BoardLine;
