31a305ba5e559452cb1a4147f0bc04dddc7979b9
ksakiyama <k.sakiyama1@gmail.com>
2018-12-23 10:23:21 +0900

    shouldComponentUpdate

--- src/Cell.js
+++ src/Cell.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { classes } from "./constants.js";
 
-export default class Cell extends React.PureComponent {
-  handleClick = (x, y) => {
-    this.props.clickHandler(x, y);
+export default class Cell extends React.Component {
+  handleClick = () => {
+    this.props.clickHandler(this.props.x, this.props.y);
   };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { type } = nextProps;
+    if (type !== this.props.type) {
+      return true;
+    }
+    return false;
+  }
 
   render() {
     return (
       <td>
-        <span
-          onClick={() => this.handleClick(this.props.x, this.props.y)}
-          className={classes[this.props.type]}
-        />
+        <span onClick={this.handleClick} className={classes[this.props.type]} />
       </td>
     );
   }
